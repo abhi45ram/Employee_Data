a8be8ed4b55aac4b59eeff61df00c85fb712fe8e
Abhishek Ramgoliyan <20bcs014@iiitdmj.ac.in>
2025-03-28 21:37:51 +0530

    added all functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import UsersPage from "./pages/UsersPage";
 import { AuthProvider } from "./context/AuthContext";
